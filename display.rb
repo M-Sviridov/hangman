8e1dca851c52f8685d45e8c573775b53a74365b3
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-21 16:43:22 +1000

    Add message when random word has been chosen and game started

--- display.rb
+++ display.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
+# require './game'
+require './colorable'
+
 # module that displays certain information for the game
 module Display
+  using Colorable
+
   def reveal_word
-    @player_guess.each { |char| print char }
+    @player_guess.each { |char| print char.fg_color(:aurora4) }
     puts ''
+  end
+
+  def game_start
+    puts "\nYour random word has been chosen and it has #{@secret_word.length} letters".fg_color(:aurora4)
+    reveal_word
   end
 end
