bde4d60296fe2df0872accc39037d620b75d77d7
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-21 17:15:11 +1000

    Change colors

--- display.rb
+++ display.rb
@@ -8,12 +8,12 @@ module Display
   using Colorable
 
   def reveal_word
-    @player_word.each { |char| print char.fg_color(:aurora4) }
+    @player_word.each { |char| print char.fg_color(:aurora3) }
     puts ''
   end
 
   def game_start
-    puts "\nYour random word has been chosen and it has #{@secret_word.length} letters".fg_color(:aurora4)
+    puts "\nYour random word has been chosen and it has #{@secret_word.length} letters".fg_color(:aurora3)
     reveal_word
   end
 end
