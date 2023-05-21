a6378bf667d2bae8af530c45a93f647a7d0ddef5
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-21 15:33:59 +1000

    Select random word from dictionary for each new Game

--- game.rb
+++ game.rb
@@ -5,10 +5,20 @@ require './instructions'
 # class representing a full game of Hangman
 class Game
   include Instructions
+  attr_accessor :secret_word
 
   def initialize
+    @dictionary = File.open('dictionary.txt', 'r')
+    @secret_word = random_word
     puts instructions
   end
-end
 
-Game.new
+  def random_word
+    array = []
+    @dictionary.each_line do |line|
+      line.chomp
+      array << line
+    end
+    array.sample
+  end
+end
