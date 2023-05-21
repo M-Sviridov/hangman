e93426c6dce2217f8b1ba9a69f0636433e701ae4
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-21 15:53:13 +1000

    Add method to set the blank word at start of the game

--- game.rb
+++ game.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 require './instructions'
+require './display'
 
 # class representing a full game of Hangman
 class Game
   include Instructions
+  include Display
+
   attr_accessor :secret_word
 
   def initialize
     @dictionary = File.open('dictionary.txt', 'r')
     @secret_word = random_word
+  end
+
+  def start
     puts instructions
   end
 
@@ -20,5 +26,13 @@ class Game
       array << line
     end
     array.sample
+  end
+
+  def set_blank_word
+    array = []
+    @secret_word.length.times do
+      array << '_'
+    end
+    array
   end
 end
